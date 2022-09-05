a0a67fcfd0be9f62bc63d163ae71401859ee907d
Jo0702 <53489735+Jo0702@users.noreply.github.com>
2022-09-05 22:40:11 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -14,6 +14,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
 import tempfile
+import pandas as pd
 import altair as alt
 
 import io
